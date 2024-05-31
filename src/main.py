67929b1109705ed09942fc79866674187ee89ca4
Isaaq-Khader <isaaqkhader@uchicago.edu>
2024-05-31 18:26:34 -0500

    Reorganize file structure

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message
 import logging
 import os
-from responses import ResponseHandler
+from src.responses import ResponseHandler
 from typing import Final
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(message)s')
