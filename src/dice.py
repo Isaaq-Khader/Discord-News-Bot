0deca4bac26529d9e1860b881fc4d05d9068b172
Isaaq-Khader <isaaqkhader@uchicago.edu>
2024-06-05 22:00:07 -0500

    Add modifier functionality to rolls

--- src/dice.py
+++ src/dice.py
@@ -7,24 +7,39 @@ logger = logging.getLogger("Dice")
 
 class Dice:
     def split_roll_text(roll: str):
-        num = re.sub("[d][0-9]+", "", roll)
+        modifier = re.sub("[0-9]+[d][0-9]+", "", roll)
+        logger.debug(f"{log.DEBUG} Modifier regex result: {modifier}")
+        if modifier:
+            modifier = int(modifier)
+            logger.debug(f"{log.DEBUG} Modifier used: {modifier}")
+            num = re.sub("[d][0-9]+[+][0-9]+", "", roll)
+            die = re.sub("[0-9]+[d]|[+][0-9]+", "", roll)
+        else:
+            logger.debug(f"{log.DEBUG} No modifier used.")
+            modifier = 0
+            num = re.sub("[d][0-9]+", "", roll)
+            die = re.sub("[0-9]+[d]", "", roll)
+
         logger.debug(f"{log.DEBUG} Number of Dice: {num}")
-
-        die = re.sub("[0-9]+[d]", "", roll)
         logger.debug(f"{log.DEBUG} Die Used: {die}")
-
-        return int(num), int(die)
+        return int(num), int(die), modifier
     
     def roll_dice(attributes: list[str]) -> str:
         logger.debug(f"{log.DEBUG} Attributes to work with: {attributes}")
         for attribute in attributes:
             if re.search("[0-9]+[d][0-9]+", attribute):
-                num, die = Dice.split_roll_text(attribute)
-                total = 0
+                num, die, mod = Dice.split_roll_text(attribute)
+                if not num or not die:
+                    logger.debug(f"{log.DEBUG} no number of dice or die side found...")
+                    return "You hooligan, the statement is not correct... please try again"
+                total = mod
+                logger.debug(f"{log.DEBUG} Initial Total: {total}")
                 for instance in range(num):
                     logger.debug(f"{log.DEBUG} Roll #{instance+1}")
-                    total += randint(1, die)
+                    roll = randint(1, die)
+                    total += roll
+                    logger.debug(f"{log.DEBUG} Rolled: {roll}")
                     logger.debug(f"{log.DEBUG} Current Total: {total}")
-                return f"You rolled [{num}d{die}]: {total}"
+                return f"You rolled [{attribute}]: {total}"
         return "Not sure what you mean there buddy."
     
